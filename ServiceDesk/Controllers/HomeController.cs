699607a5301268e391d91da9ea1efe90b1305b90
Shashwatik Sinha <Shashwatik.Sinha@WinWire.com>
2021-10-21 14:45:14 +0530

    updated to home controller and dbo

--- ServiceDesk/Controllers/HomeController.cs
+++ ServiceDesk/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ServiceDesk.Models;
 
 namespace ServiceDesk.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         [HttpGet]
         public ActionResult Index()
         {
@@ -17,7 +19,23 @@ namespace ServiceDesk.Controllers
         [HttpPost]
         public ActionResult Index(int username, string password)
         {
-            
+            WebAPIDBO a = new WebAPIDBO();
+            bool validlogin = a.validateLogin(username,password);
+
+            if(validlogin == false)
+            {
+                ViewBag.msg = ("Wrong Credentials");
+                return View("Index");
+            }
+
+            else
+            {
+                Session["empid"] = username;
+                Session["role"] = a.getRole(username);
+
+                
+            }
+
 
             string user_role = "sdfasd";
             if (user_role == "user") return RedirectToAction("Users", "Role");
--- ServiceDesk/Models/WebAPIDBO.cs
+++ ServiceDesk/Models/WebAPIDBO.cs
@@ -7,6 +7,14 @@ namespace ServiceDesk.Models
 {
     public class WebAPIDBO
     {
+        internal bool validateLogin(int username, string password)
+        {
+            throw new NotImplementedException();
+        }
 
+        internal string getRole(int username)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
