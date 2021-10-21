4fea432a77f9b0dc85028b57a25421e71dc38f7f
Dhanush Narayanaswamy <Dhanush.Narayanaswamy@WinWire.com>
2021-10-21 20:44:50 +0530

    WebApiworking

--- ServiceDesk/Controllers/HomeController.cs
+++ ServiceDesk/Controllers/HomeController.cs
@@ -1,28 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using ServiceDesk.Models;
 
 namespace ServiceDesk.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet]
+        emp1 E = new emp1();
         public ActionResult Index()
         {
+           
             return View();
         }
 
         [HttpPost]
-        public ActionResult Index (string empid, string pass_word)
+        public ActionResult Index (int empid, string pass_word)
         {
+            emp1 E = new emp1();
+            using (var client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("https://localhost:44336/api/");
+                client.DefaultRequestHeaders.Clear();
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                string s= "values/" + empid;
+                var Res = client.GetAsync(s);
+                Res.Wait();
+                var result = Res.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    var EmpResponse = result.Content.ReadAsStringAsync().Result;
+                    //EmpResponse.Wait();
+                    E = JsonConvert.DeserializeObject<emp1>(EmpResponse);
+                    // = EmpResponse.Result;
+                }
+                else
+                {
+
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+
+            }
+
+            if (E.Empid==empid && E.Password==pass_word)
+            {
+                if (E.Role_assigned == "Users") return RedirectToAction("Users", "Role");
+                    else if (E.Role_assigned== "Lead") return RedirectToAction("Lead", "Role");
+                 else if (E.Role_assigned == "Manager") return RedirectToAction("Manager", "Role");
+                else return RedirectToAction("Admin", "Role");
+            }
+            else
+            {
+                ViewBag.msg = "Wrong Credentials";
+                return View();
+            }
+                
             
-            string user_role = "sdfasd";
-            if(user_role == "user") return RedirectToAction("Users","Role");
-            else if(user_role == "lead") return RedirectToAction("Lead", "Role");
-            else if (user_role == "manager") return RedirectToAction("Manager","Role");
-            return RedirectToAction("Admin", "Role");
+            
         }
 
         public ActionResult About()
---
+++ ServiceDesk/Models/emp1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceDesk.Models
+{
+    public class emp1
+    {
+        public int Empid{ get; set; }
+     public string Password{ get; set; }
+        public string Role_assigned{ get; set; }
+        public string des { get; set; }
+    }
+}
