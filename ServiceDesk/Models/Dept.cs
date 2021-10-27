9d24eb70c3cc04bbed1d3c11b0feb24b02be6e09
Dhanush Narayanaswamy <Dhanush.Narayanaswamy@WinWire.com>
2021-10-27 11:25:58 +0530

    Commitdept

---
+++ ServiceDesk/Models/Dept.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceDesk.Models
+{
+    public class Dept
+    {
+
+        public int Dept_ID { get; set; }    
+        public string Dept_Name { get; set; }
+        public Nullable<int> Manager_ID { get; set;}
+    }
+
+    }
+}
