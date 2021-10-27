d31c950daaea48f7065857c0d51020f8f377c8e2
Dhanush Narayanaswamy <Dhanush.Narayanaswamy@WinWire.com>
2021-10-27 20:57:54 +0530

    DBO

--- ServiceDesk/Controllers/HomeController.cs
+++ ServiceDesk/Controllers/HomeController.cs
@@ -22,68 +22,36 @@ namespace ServiceDesk.Controllers
 
         public ActionResult Index()
         {
-           return View();
+            return View();
         }
 
         [HttpPost]
 
         public ActionResult Index(int empid, string pass_word)
         {
-
-
+            //WebAPIDBO dbo = new WebAPIDBO();
+            //bool check = dbo.validateLogin(empid, pass_word);
+            //if (check == true)
+            //{
+            //    Session["Employee"] = dbo.getProfile(empid);
+            //    return RedirectToAction("StartRoute", "Role");
+            //}
+            //else
+            //{
+            //    return View();
+            //}
             WebAPIDBO dbo = new WebAPIDBO();
-            bool check = dbo.validateLogin(empid,pass_word);
-            
+            Employee E=new Employee();
+            E = dbo.getProfile(empid);
 
-            if (check == true)
-            {
-                Session["Employee"] = dbo.getProfile(empid);
-                return RedirectToAction("StartRoute", "Role");
-            }
-            else
-            {
-                return View();
-            }
-
-            
-            /*
-            Employee E = new Employee();
-            using (var client = new HttpClient())
-            {
-
-                client.BaseAddress = new Uri("https://localhost:44336/api/");
-                client.DefaultRequestHeaders.Clear();
-
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                string s= "values/" + empid;
-                var Res = client.GetAsync(s);
-                Res.Wait();
-                var result = Res.Result;
-                if (result.IsSuccessStatusCode)
-                {
-
-                    var EmpResponse = result.Content.ReadAsStringAsync().Result;
-                    //EmpResponse.Wait();
-                    E = JsonConvert.DeserializeObject<Employee>(EmpResponse);
-                    // = EmpResponse.Result;
-                }
-                else
-                {
-
-
-                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
-                }
-
-            }
-
-            if (E.Empid==empid && E.Password==pass_word)
+            if (E.Emp_ID==empid && E.Emp_Pwd==pass_word)
             {
                 Session["Employee"] = E;
                 Session["role"] = "User";
 
-                if (E.Role_assigned == "Users") return RedirectToAction("Users", "Role");
-                    else if (E.Role_assigned== "Lead") return RedirectToAction("Lead", "Role");
-                 else if (E.Role_assigned == "Manager") return RedirectToAction("Manager", "Role");
+                if (E.Emp_Role== "Users") return RedirectToAction("Users", "Role");
+                    else if (E.Emp_Role== "Lead") return RedirectToAction("Lead", "Role");
+                 else if (E.Emp_Role == "Manager") return RedirectToAction("Manager", "Role");
                 else return RedirectToAction("Admin", "Role");
             }
             else
@@ -94,5 +62,6 @@ namespace ServiceDesk.Controllers
             
         }
 
-       
+
+        }
     }
