8bc6b77721a2829647cf079bb6ae587b8c2ee0af
Shashwatik Sinha <Shashwatik.Sinha@WinWire.com>
2021-10-28 09:40:59 +0530

    sdasd

--- ServiceDesk/Controllers/HomeController.cs
+++ ServiceDesk/Controllers/HomeController.cs
@@ -45,53 +45,6 @@ namespace ServiceDesk.Controllers
                 return View();
             }
 
-
-            /*
-            Employee E = new Employee();
-            using (var client = new HttpClient())
-            {
-
-                client.BaseAddress = new Uri("https://localhost:44336/api/");
-                client.DefaultRequestHeaders.Clear();
-
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                string s= "values/" + empid;
-                var Res = client.GetAsync(s);
-                Res.Wait();
-                var result = Res.Result;
-                if (result.IsSuccessStatusCode)
-                {
-
-                    var EmpResponse = result.Content.ReadAsStringAsync().Result;
-                    //EmpResponse.Wait();
-                    E = JsonConvert.DeserializeObject<Employee>(EmpResponse);
-                    // = EmpResponse.Result;
-                }
-                else
-                {
-
-
-                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
-                }
-
-            }
-
-            if (E.Empid==empid && E.Password==pass_word)
-            {
-                Session["Employee"] = E;
-                Session["role"] = "User";
-
-                if (E.Role_assigned == "Users") return RedirectToAction("Users", "Role");
-                    else if (E.Role_assigned== "Lead") return RedirectToAction("Lead", "Role");
-                 else if (E.Role_assigned == "Manager") return RedirectToAction("Manager", "Role");
-                else return RedirectToAction("Admin", "Role");
-            }
-            else
-            {
-                ViewBag.msg = "Wrong Credentials";
-                return View();
-            }*/
-
         }
 
     }
